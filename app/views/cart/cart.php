4c286d9b5ccf975338cdd951b0c3a9fba60dcc3e
Benjamin Shanahan <benshanahan1@gmail.com>
2014-12-27 03:11:04 -0500

    Added cart.php.

---
+++ app/views/cart/cart.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ben
+ * Date: 12/27/14
+ * Time: 2:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$user = $_POST['user'];
+
+echo "Hello, {$user}, welcome to your cart.";
+
+?>
