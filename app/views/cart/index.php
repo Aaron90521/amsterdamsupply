b5868b4cab3e9e8d5594042920a6b045b18ebf49
Aaron90521 <Aaron90521@gmail.com>
2014-12-27 02:42:12 -0500

    Added index for cart view.

---
+++ app/views/cart/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Joel
+ * Date: 12/27/14
+ * Time: 2:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "hello world";
+?>
